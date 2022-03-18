1af8d0a980b443eef573516f0d1797752bd53faa
kyler <7578101@isd535.org>
2022-03-18 11:21:51 -0500

    dhrdrhd

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
 // Put your JavaScript here
+(function (window) {
+    'use strict';
+    let App = window.App || {};
+    function DataStore() {
+        console.log('Running the DataStore function...')
+    }
+    
+    DataStore.prototype.add = function (key, val) {
+        this.data[key] = val;
+    };
+
+    App.DataStore = DataStore;
+    window.App = App;
+})(window);
+
+function DataStore() {
+    console.log('Running the DataStore function...')
+    this.data = {};
+}
+
